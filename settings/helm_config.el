8759473e7e08dee2dfbeb6e7ee409c491a6a7aa1
Sebastian Hahn <sebastian.hahn@gmail.com>
2014-11-27 01:04:38 +0100

    added projectile settings

--- settings/helm_config.el
+++ settings/helm_config.el
@@ -8,15 +8,21 @@
 ;; accidentially pressed "C-x C-c"
 (setq helm-command-prefix-key "C-c h")
 
-;; (require-package 'helm-projectile)
-;; (require 'helm-projectile)
-;; ;; custom function for finding and switching projects
+(require-package 'helm-projectile)
+(require 'helm-projectile)
+;; custom function for finding and switching projects
+
+(projectile-global-mode)
+(setq projectile-completion-system 'helm)
+(helm-projectile-on)
+
 ;; (defun cp/projectile-find ()
-;;   "if not in a project switch to project"
-;;   (interactive)
-;; (if (projectile-project-p)
-;;     (helm-projectile)
-;;     (helm-projectile-switch-project)))
+;;  "if not in a project switch to project"
+;;  (interactive)
+;;  (if (projectile-project-p)
+;;      (helm-projectile)
+;;      (helm-projectile-switch-project)))
+
 ;; (global-set-key (kbd "C-x f") 'cp/projectile-find)
 
 (require 'helm-config)
@@ -47,7 +53,7 @@
  helm-candidate-number-limit 200 ; limit the number of displayed canidates
  helm-M-x-requires-pattern 0     ; show all candidates when set to 0
  helm-boring-file-regexp-list
- '("\\.git$" "\\.hg$" "\\.svn$" "\\.CVS$" "\\._darcs$" "\\.la$" "\\.o$" "\\.i$") ; do not show these files in helm buffer
+ '("\\.pyc$" "\\.git$" "\\.hg$" "\\.svn$" "\\.CVS$" "\\._darcs$" "\\.la$" "\\.o$" "\\.i$") ; do not show these files in helm buffer
  helm-ff-file-name-history-use-recentf t
  helm-move-to-line-cycle-in-source t ; move to end or beginning of source
                                         ; when reaching top or bottom of source.
